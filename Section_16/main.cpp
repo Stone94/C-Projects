78c8150055e34a8d46da1c86bf5dd67db6445cc5
Perry <perry.collins1994@gmail.com>
2018-06-20 17:54:48 -0300

    Infinite Loops

---
+++ Section_16/main.cpp
@@ -0,0 +1,25 @@
+// Infinite Loops
+#include <iostream>
+
+
+int main(){
+// ///////////////INFINITE FOR LOOP//////////////////////////////////
+// for (;;)
+//   std::cout << "Print forever!" << std::endl;
+// ///////////////INFINITE WHILE LOOP///////////////////////////////
+// while (true)
+// char again{};
+//   std::cout << "Do you want to loop again? (Y/N): ";
+// cin >> again;
+//
+// if (again == 'N' || again == 'n')
+//     break;
+// ///////////////INFINITE DO WHILE LOOP////////////////////////////
+// do {
+//     std::cout << "Print forever Again!" << std::endl;
+// } while (true);
+// ////////////////////////////////////////////////////////////////
+
+
+
+}
